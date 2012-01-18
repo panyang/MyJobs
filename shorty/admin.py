3fbcc20df0949468b02d952b1ef0d6d49d0fea1b
Mike Seidle <mike@directemploers.org>
2012-01-18 14:32:25 -0500

    Remove vestigal code

--- shorty/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from shorty.models import Shorty, Click
-
-
-class ShortyOption(admin.ModelAdmin):
-    """Default shorty admin defs"""
-    list_display = ('id', 'created', 'shorty_url', 'from_url', 'to_url')
-
-admin.site.register(Shorty, ShortyOption)
