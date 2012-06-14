5233db7e66a9dbd52babb9b63f9580b3b176164f
Jason Sole <jason@directemployersfoundation.org>
2012-06-14 21:01:21 +0000

    Update static references to use nlx.org. MJ-2

--- default_settings.py
+++ default_settings.py
@@ -36,10 +36,10 @@ USE_I18N = True
 USE_L10N = True
 
 MEDIA_ROOT = os.path.join(_PATH, 'files', 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = 'http://src.nlx.org/myjobs/admin/'
 
 STATIC_ROOT = os.path.join(_PATH, 'files', 'static')
-STATIC_URL = '/static/'
+STATIC_URL = 'http://src.nlx.org/myjobs/'
 STATICFILES_DIRS = (
     os.path.join(_PATH, 'static'),
 )
@@ -48,34 +48,10 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = abspath(join(PROJ_ROOT, 'files', 'static'))
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
-
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-)
-
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
+ADMIN_MEDIA_PREFIX = 'http://src.nlx.org/myjobs/admin/'
 
 
 TEMPLATE_LOADERS = (
@@ -157,9 +133,7 @@ LOGOUT_REDIRECT_URL = '/home'
 
 MANAGERS = ADMINS
 
-ADMIN_MEDIA_PREFIX = '/static/admin/'
-
-DATABASES = {'default': {'NAME': 'directseo','ENGINE': 'django.db.backends.sqlite3','USER': '','PASSWORD': '','HOST': '','PORT': '',}}
+#DATABASES = {'default': {'NAME': 'directseo','ENGINE': 'django.db.backends.sqlite3','USER': '','PASSWORD': '','HOST': '','PORT': '',}}
 #DATABASES = {
 #    'default': {
 #        'NAME': 'myjobs',
