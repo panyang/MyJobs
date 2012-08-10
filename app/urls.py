bc00d8fd5821b948b1d94e477ca87db2805b6e4e
Ani Chan <ani.chan844@gmail.com>
2012-08-09 21:24:18 -0400

    Refactor url config.

--- app/urls.py
+++ app/urls.py
@@ -1,23 +1,23 @@
 from django.conf.urls.defaults import patterns, url, include
 from django.contrib import admin
 from django.http import HttpResponsePermanentRedirect
-from app.views import home, done, logout, error, about, privacy, password_connection
+from app.views import home, done, logout, error,about,privacy, password_connection
 
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^$', lambda request: HttpResponsePermanentRedirect('http://jobs.jobs')),
-    url(r'^home/$', home, name='home'),
-    url(r'^about/$', about, name='about'),
-    url(r'^privacy/$', privacy, name='privacy'),     
-    url(r'^done/$', done, name='done'),
-    url(r'^error/$', error, name='error'),
-    url(r'^logout/$', logout, name='logout'),
-    url(r'^lp/$', password_connection, name='lostpassword'),
+urlpatterns = patterns('MyJobs.app.views',
+    #url(r'^$', lambda request: HttpResponsePermanentRedirect('http://jobs.jobs')),
+    url(r'^$', 'home', name='home'),
+    url(r'^about/$', 'about', name='about'),
+    url(r'^privacy/$', 'privacy', name='privacy'),     
+    url(r'^done/$', 'done', name='done'),
+    url(r'^error/$', 'error', name='error'),
+    url(r'^logout/$', 'logout', name='logout'),
+    url(r'^lp/$', 'password_connection', name='lostpassword'),
     url(r'^admin/', include(admin.site.urls)),
     # social_auth urls
     url(r'', include('social_auth.urls')),
     # django_registration urls
-    (r'^profile/', include('registration.urls')),
+    (r'^accounts/', include('registration.urls')),
 )
