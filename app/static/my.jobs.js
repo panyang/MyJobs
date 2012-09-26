528b3c539dbc5ab8a0e41e488a120d1cf3caa571
fezick <jason@directemployersfoundation.org>
2012-09-26 16:49:37 -0400

    Move css to my.jobs.css, document javascript. MS-430

--- app/static/my.jobs.js
+++ app/static/my.jobs.js
@@ -1,3 +1,6 @@
+/******
+My.jobs Share window functions. Assigns click events and builds share window. 
+*******/
 $('#twitter').live('click',function() {        
     q=location.href;
     share_url = '/auth/twitter/?&url='+encodeURIComponent(q);
@@ -13,6 +16,19 @@ $('#linkedin').live('click',function() {
     share_url = '/auth/linkedin/?&url='+encodeURIComponent(q);
     openShareWindow(share_url,"LinkedIn");
 });
-function openShareWindow(url,name){    
-    window.open(url, 'Share this on '+name,'toolbar=no,width=568,height=360');
+function openShareWindow(url,name){
+    /*
+    Opens a new window using OAuth to share content.
+    
+    Inputs:
+        :url:   The share url to use
+        :name:  The name of the social network
+    
+    Returns:
+        None - opens a new window.
+    
+    */
+    title = 'Share this on '+name;
+    atts = 'toolbar=no,width=568,height=360';
+    share_window = window.open(url, title,atts);
 }
