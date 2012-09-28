066739c79fd2797ceac453a416b8720028895b85
Ani Chan <ani.chan844@gmail.com>
2012-09-28 10:37:13 -0400

    Change settings back to default

--- settings.py
+++ settings.py
@@ -7,33 +7,12 @@ from secrets import PROD_DB_PASSWD
 DEBUG = True
 
 DATABASES = {
-    'default':
-    {'NAME': 'directseo',
-     'ENGINE': 'django.db.backends.sqlite3',
-     'USER': '','PASSWORD': '',
-     'HOST': '',
-     'PORT': '',}
+    'default': {
+        'NAME': 'myjobs1',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'db_mjuser',
+        'PASSWORD': PROD_DB_PASSWD,
+        'HOST': 'myjobs.c9shuxvtcmer.us-east-1.rds.amazonaws.com',
+        'PORT': '3306'
+    },
 }
-
-# DATABASES = {
-#     'default': {
-#         'NAME': 'myjobs1',
-#         'ENGINE': 'django.db.backends.mysql',
-#         'USER': 'db_mjuser',
-#         'PASSWORD': PROD_DB_PASSWD,
-#         'HOST': 'myjobs.c9shuxvtcmer.us-east-1.rds.amazonaws.com',
-#         'PORT': '3306'
-#     },
-# }
-
-#DATABASES = {
-#    'default': {
-#        'NAME': 'myjobs',
-#        'ENGINE': 'django.db.backends.mysql',
-#        'USER': '',
-#        'PASSWORD': '',
-#        'HOST': '',
-#        'PORT': '3306'
-#    },
-#}
-
