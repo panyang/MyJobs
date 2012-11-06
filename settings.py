55a653ad834c3fe27c49f7b783c021b6e3e84a85
Ani Chan <ani.chan844@gmail.com>
2012-11-06 12:36:56 -0500

    Initial migrations with south

--- settings.py
+++ settings.py
@@ -8,21 +8,21 @@ from secrets import PROD_DB_PASSWD
  
 DEBUG = True
  
-# DATABASES = {
-#     'default': {
-#         'NAME': 'myjobs1',
-#         'ENGINE': 'django.db.backends.mysql',
-#         'ENGINE': 'django.db.backends.mysql',
-#         'USER': 'db_mjuser',
-#         'PASSWORD': PROD_DB_PASSWD,
-#         'HOST': 'myjobs.c9shuxvtcmer.us-east-1.rds.amazonaws.com',
-#         'PORT': '3306'
-#     },
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'myjobs'
+        'NAME': 'myjobs1',
+        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'db_mjuser',
+        'PASSWORD': PROD_DB_PASSWD,
+        'HOST': 'myjobs.c9shuxvtcmer.us-east-1.rds.amazonaws.com',
+        'PORT': '3306'
     },
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'myjobs'
+#     },
+# }
