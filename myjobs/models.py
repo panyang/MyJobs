ac737910faa748ef54afb232e19714e71bae0c61
Ani Chan <ani.chan844@gmail.com>
2012-11-29 14:27:26 -0500

    Fix superuser test

--- myjobs/models.py
+++ myjobs/models.py
@@ -1,7 +1,7 @@
 import datetime
 
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Group, Permission
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.core.mail import send_mail
 from django.utils.translation import ugettext_lazy as _
 from social_auth.signals import pre_update
