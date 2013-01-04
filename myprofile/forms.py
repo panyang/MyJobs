ccd723ebac7dee159591c0ea0e7d19044d9d2af5
Ani Chan <ani.chan844@gmail.com>
2013-01-04 18:31:59 -0500

    Add comments to describe new form functionality

--- myprofile/forms.py
+++ myprofile/forms.py
@@ -3,6 +3,19 @@ from myprofile.models import *
 
 
 class BaseProfileForm(forms.ModelForm):
+    """
+    All ProfileUnit forms inherit from this model. It takes a user
+    object as an initial input from the views and saves the form instance
+    to that specified user.
+
+    :Inputs:
+    user - we will always pass a user object in because all ProfileUnits
+    are linked to a user
+    auto_id - this is a boolean that determines whether a label is displayed or
+    not and is by default set to True. If we want the placeholder text to be
+    displayed instead, pass in the value as False
+    
+    """
     def __init__(self, *args, **kwargs):
         self.user = kwargs.pop('user',None)
         super (BaseProfileForm, self).__init__(*args, **kwargs)
@@ -15,6 +28,16 @@ class BaseProfileForm(forms.ModelForm):
 
         
 class NameForm(BaseProfileForm):
+    # For most fields, we can set the model in the Meta class to automatically
+    # create the form fields. Since we want to alter the field name from the
+    # default created by Django from the db field names, we have to define
+    # these manually
+
+    # We will always define the placeholder text for our formfields because
+    # there are different instances where we'll use the label or we'll use
+    # just the placeholder. The distinction is dealt with in the
+    # form-error-highlight.html template and by passing in the auto_id value
+    # as described above
     given_name = forms.CharField(label="First Name", max_length=30,
                                  widget=forms.TextInput(attrs={
                                      'placeholder': 'First Name'}))
@@ -26,6 +49,8 @@ class NameForm(BaseProfileForm):
 
     class Meta:
         model = Name
+        # Exclude any fields from ProfileUnits or Name that doesn't require user
+        # input
         exclude = ('user', 'date_created', 'date_updated', 'content_type',
         'display_name')
 
@@ -35,7 +60,9 @@ class SecondaryEmailForm(BaseProfileForm):
         model = SecondaryEmail
         exclude = ('user', 'date_created', 'date_updated', 'content_type',
                    'verified_date')
+        # Here, we don't need to change the label but want to just change the
+        # widget. We can alter just the widget by passing this dictionary
         widgets = {
             'email': forms.TextInput(attrs={'placeholder': 'Email'}),
-            'label': forms.TextInput(attrs={'placeholder': 'labeel'})
+            'label': forms.TextInput(attrs={'placeholder': 'Label'})
         }
