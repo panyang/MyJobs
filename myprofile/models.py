8e5d5ee5387148391ca78a5d58f3baff0a34b4fd
JustinDennis <justin@directemployersfoundation.org>
2013-01-14 14:26:14 -0500

    Updated Telephone Profile

--- myprofile/models.py
+++ myprofile/models.py
@@ -87,8 +87,9 @@ class Telephone(ProfileUnits):
     number = models.CharField(max_length=8, verbose_name="DialNumber")
     extension = models.IntegerField(max_length=5, blank=True)
     
-    def get_channel_code(self):
-    	    if self.use_code = "Home" or self.use_code = "Work" or self.use_code = "Other"
+    #Checks use_code and returns channel_code     
+    def get_channel_code(self):    	       	    
+	    if self.use_code = "Home" or self.use_code = "Work" or self.use_code = "Other"
     	    	return "Telephone"
     	    if self.use_code = "Mobile"
     	    	return "MobileTelephone"
