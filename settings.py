4a93d265181e7ef21a510103e8e1c7a6c9ac1505
fezick <jason@directemployersfoundation.org>
2013-01-15 12:08:48 -0500

    Add generalixed settings file with production DB.

---
+++ settings.py
@@ -0,0 +1,26 @@
+from default_settings import *
+from secrets import PROD_DB_PASSWD
+ 
+DEBUG = True
+ 
+DATABASES = {
+    'default': {
+        'NAME': 'myjobs1',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'db_mjuser',
+        'PASSWORD': PROD_DB_PASSWD,
+        'HOST': 'myjobs.c9shuxvtcmer.us-east-1.rds.amazonaws.com',
+        'PORT': '3306'
+    },
+}
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
+try: #pull in local settings, if it exists. Primarily only for development.
+    from local_settings import *
+except:
+    pass
+
