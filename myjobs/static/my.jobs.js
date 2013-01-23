727990b6d63333edf366fd72876809bfb3cb0b08
fezick <jason@directemployersfoundation.org>
2013-01-23 16:48:31 -0500

    Add json and jsonp files. Add default values and label changes for regions. MJ-93

--- myjobs/static/my.jobs.js
+++ myjobs/static/my.jobs.js
@@ -23,7 +23,6 @@ $('#linkedin').live('click',function() {
     openShareWindow(share_url,"LinkedIn");
 });
 $(document).ready(function(){
-    console.log("ready");
     $(".topbar .main-nav").click(function(){
         $("#nav").toggleClass("active");
         return false;
@@ -55,164 +54,167 @@ function repopulateSelectField(field_id, new_data, use_combobox){
     $("#"+field_id).html(option_string);
     //$("#"+field_id+" + .ui-combobox").remove();
     $("#"+field_id).combobox();
-    console.log(use_combobox)
 }
 
 /*Combobox Widget. Based loosely on the jQuery UI example*/
-(function($){
-    $.widget( "ui.makeCombobox",{
-        _create: function(){
-            parent = this.element;
-            selected = parent.children( ":selected" )
-            parent_id = parent.attr("id");
-            parent_id_orig = parent_id+"_orig";
-            parent.attr("id",parent_id_orig)
-            parent.hide();
-            new_ac = $("<input>")
-                .attr("id",parent_id)
-                .attr("type","text")
-                .attr("value",selected.html())
-                .addClass("comboboxWidget")
-                .addClass(parent.attr("class"))
-                .insertBefore(parent)
-                .autocomplete({
-                    source: function(req,resp){
-                        /****
-                        Dynamically build the AC list from parent select list.
-                        Doing it this way allows for the AC to pick up changes
-                        in the option list made by ajax.
-                        ****/
-                        dict = [];
-                        source = $("#"+this.element.attr("id")+"_orig").children("option");
-                        for(i=0;i<source.length;i++){
-                            label = source[i].innerHTML;
-                            value = source[i].value;
-                            if(source[i].value != ""){
-                            // if the search phrase is in the label
-                                if(label.toLowerCase().indexOf(req.term.toLowerCase())!=-1){
-                                    dict.push({"label":label,"value":label});
-                                }
-                            }
+(function($){$.widget( "ui.makeCombobox",{
+    _create: function(){
+        parent = this.element;
+        selected = parent.children( ":selected" )
+        parent_id = parent.attr("id");
+        parent_id_orig = parent_id+"_orig";
+        parent.attr("id",parent_id_orig)
+        parent.hide();
+        new_ac = $("<input>")
+            .attr("id",parent_id)
+            .attr("type","text")
+            .attr("value",selected.html())
+            .addClass("comboboxWidget")
+            .addClass(parent.attr("class"))
+            .insertBefore(parent)
+            .autocomplete({
+                source: function(req,resp){
+                    /****
+                    Dynamically build the AC list from parent select list.
+                    Doing it this way allows for the AC to pick up changes
+                    in the option list made by ajax.
+                    ****/
+                    dict = [];
+                    source = $("#"+this.element.attr("id")+"_orig").children("option");
+                    for(i=0;i<source.length;i++){
+                        label = source[i].innerHTML;
+                        value = source[i].value;
+                        if(source[i].value != "" &&
+                           label.toLowerCase().indexOf(req.term.toLowerCase())!=-1){
+                                dict.push({"label":label,"value":label});                            
                         }
-                        resp(dict);
-                    },
-                    minLength: 0,
-                    select: function(event,ui){
+                    }
+                    resp(dict);
+                },
+                minLength: 0,
+                select: function(event,ui){
+                    /****
+                    On select, get the value of the field, and check if
+                    the field has a dependent region list. If so, get that
+                    listing from jsonp and populate the select list with the
+                    the new data.                        
+                    ****/
+                    my_parent = $("#"+this.id+"_orig");
+                    if(my_parent.hasClass("hasRegions")){
                         /****
-                        On select, get the value of the field, and check if
-                        the field has a dependent region list. If so, get that
-                        listing from jsonp and populate the select list with the
-                        the new data.                        
+                        This is logic specific to the region select list
+                        when the combo box is being applied to a country
+                        select list that also has a region list assigned
+                        to it. The majority of the time, this will be due
+                        to the use of the {% country_region_select %}
+                        template tag.
                         ****/
-                        my_parent = $("#"+this.id+"_orig");
-                        if(my_parent.hasClass("hasRegions")){
-                            /****
-                            This is logic specific to the region select list
-                            when the combo box is being applied to a country
-                            select list that also has a region list assigned
-                            to it. The majority of the time, this will be due
-                            to the use of the {% country_region_select %}
-                            template tag.
-                            ****/
-                            orig_options = my_parent.children("option")
-                            val_to_get = "";
-                            for(opt=0; opt < orig_options.length; opt++){
-                                if (ui.item.label==$(orig_options[opt]).html()){
-                                    val_to_get = $(orig_options[opt]).val()
-                                }
-                            }       
-                            // build the url for the jsonp ajax call
-                            region_url = "http://js.nlx.org/myjobsdata/";
-                            region_url+= val_to_get.toLowerCase();
-                            region_url+= "_regions.jsonp";
-                            //temp hide the region select in case of 404
-                            $("#region_selection + a").hide()
-                            $("#region_selection").hide()
-                            //make the ajax call for region data
-                             $.ajax({
-                                url: region_url,
-                                dataType: "jsonp",
-                                data: {},
-                                // the jsonp files are static, so the callback
-                                // must be set manually, not by jQuery
-                                jsonpCallback: "returnRegionData",
-                                success: function(data){
-                                    // replace the <option>s for the parent.
-                                    // the widget will take note and update
-                                    // itself
-                                    var opts = ""
-                                    for(var key in data){
-                                        value = data[key];
-                                        opts+="<option value="+key+">";
-                                        opts+=value+"</option>";
-                                    }                                    
-                                    if(opts!=""){
-                                        $("#region_selection_orig").html(opts);
-                                        // turn on the region widget
-                                        $("#region_selection + a").show()
-                                        $("#region_selection").show()
-                                        //set widget default value
-                                        $("#region_selection").val(
-                                            $("#region_selection_orig")
-                                            .children(":selected").html()
-                                            );
-                                    }
-                                },
-                                error:function(x,str,err){
-                                    console.log("error");
-                                    $("#region_selection + a").hide()
-                                    $("#region_selection").hide()
-                                }
-                            });                         
-                        }
-                    },
-                    change: function(){
-                        /*****
-                        Link the original select box to the dynamic AC field
-                        *****/
-                        orig_options = $("#"+this.id+"_orig").children("option")
-                        ac_field = this;
-                        val_to_select = "";
+                        orig_options = my_parent.children("option")
+                        val_to_get = "";
                         for(opt=0; opt < orig_options.length; opt++){
-                            if (ac_field.value==$(orig_options[opt]).html()){
-                                val_to_select = $(orig_options[opt]).val()
+                            if (ui.item.label==$(orig_options[opt]).html()){
+                                val_to_get = $(orig_options[opt]).val()
                             }
+                        }       
+                        // build the url for the jsonp ajax call
+                        region_url = "http://js.nlx.org/myjobsdata/";
+                        region_url+= val_to_get.toLowerCase();
+                        region_url+= "_regions.jsonp";
+                        //temp hide the region select in case of 404
+                        $("label[for=region_selection]").hide()
+                        $("#region_selection + a").hide()
+                        $("#region_selection").hide()
+                        $("#region_selection_orig").val("none")
+                        //make the ajax call for region data
+                         $.ajax({
+                            url: region_url,
+                            dataType: "jsonp",
+                            data: {},
+                            // the jsonp files are static, so the callback
+                            // must be set manually, not by jQuery
+                            jsonpCallback: "returnRegionData",
+                            success: function(data){
+                                // replace the <option>s for the parent.
+                                // the widget will take note and update
+                                // itself
+                                var opts = ""
+                                for(var i in data.regions){
+                                    item = data.regions[i]
+                                    opts_attrs = "value='"+item.code+"'";
+                                    if(item.code.toLowerCase()==data.default_option.toLowerCase()){
+                                        opts_attrs += " SELECTED";
+                                    }
+                                    opts+="<option "+opts_attrs+">";
+                                    opts+=item.name+"</option>";
+                                }
+                                if(typeof(data.friendly_label)!="undefined"){
+                                    label = data.friendly_label;
+                                }else{
+                                    label = "Region";
+                                }
+                                if(opts!=""){
+                                    $("#region_selection_orig").html(opts);
+                                    // turn on the region widget
+                                    $("label[for=region_selection]").html(label)
+                                    $("label[for=region_selection]").show()
+                                    $("#region_selection + a").show()
+                                    $("#region_selection").show()
+                                    //set widget default value
+                                    $("#region_selection").val(
+                                        $("#region_selection_orig")
+                                        .children(":selected").html()
+                                        );
+                                }
+                            }
+                        });                         
+                    }
+                },
+                change: function(){
+                    /*****
+                    Link the original select box to the dynamic AC field
+                    *****/
+                    orig_options = $("#"+this.id+"_orig").children("option")
+                    ac_field = this;
+                    val_to_select = "";
+                    for(opt=0; opt < orig_options.length; opt++){
+                        if (ac_field.value==$(orig_options[opt]).html()){
+                            val_to_select = $(orig_options[opt]).val()
                         }
-                        my_parent = $("#"+this.id+"_orig");
-                        my_parent.val(val_to_select);
-                        //TODO make sure this is getting set right after changes to select
                     }
-                });
-            $("<a>")
-                .attr( "title", "Show All Items")
-                .attr( "data-for",parent_id)
-                .insertAfter(new_ac)
-                .button({
-                    icons: {
-                        primary: "ui-icon-triangle-1-s"
-                    },
-                    text: false
-                })
-                .addClass( "ui-corner-right ui-combobox-toggle" )
-                .click(function() {
-                    // close if already visible
-                    if ( new_ac.autocomplete( "widget" ).is( ":visible" ) ) {
-                        new_ac.autocomplete( "close" );
-                        return;
-                    }
-    
-                    // work around a bug (likely same cause as #5265)
-                    $( this ).blur();
-    
-                    // pass empty string as value to search for, displaying all results
-                    target = $(this).attr("data-for");
-                    $("#"+target).autocomplete( "search", "" );
-                    $("#"+target).focus();
-                });
-        },
-        destroy: function() {
-            this.element.show();
-            $.Widget.prototype.destroy.call( this );
-        }
-    })
-})( jQuery )
+                    my_parent = $("#"+this.id+"_orig");
+                    my_parent.val(val_to_select);
+                    //TODO make sure this is getting set right after changes to select
+                }
+            });
+        $("<a>")
+            .attr( "title", "Show All Items")
+            .attr( "data-for",parent_id)
+            .insertAfter(new_ac)
+            .button({
+                icons: {
+                    primary: "ui-icon-triangle-1-s"
+                },
+                text: false
+            })
+            .addClass( "ui-corner-right ui-combobox-toggle" )
+            .click(function() {
+                // close if already visible
+                if ( new_ac.autocomplete( "widget" ).is( ":visible" ) ) {
+                    new_ac.autocomplete( "close" );
+                    return;
+                }
+
+                // work around a bug (likely same cause as #5265)
+                $( this ).blur();
+
+                // pass empty string as value to search for, displaying all results
+                target = $(this).attr("data-for");
+                $("#"+target).autocomplete( "search", "" );
+                $("#"+target).focus();
+            });
+    },
+    destroy: function() {
+        this.element.show();
+        $.Widget.prototype.destroy.call( this );
+    }
+})})( jQuery )
--- myprofile/templatetags/selection_boxes.py
+++ myprofile/templatetags/selection_boxes.py
@@ -6,25 +6,34 @@ from django.utils import simplejson as json
 register = template.Library()
 
 @register.simple_tag
-def country_region_select(selected="usa", html_id="", input_name="country"):
+def country_region_select(selected="can", html_id="", input_name="country"):
+    """
+    Build a Country/Region Section for a form.
+    :selected:      default key to select
+    :html_id:       html id to use for the element
+    :input_name:    the form element name for country. Region name is locked
+                    in order to provide javascript functionality.
+    
+    """
     country_tag = country_select(selected,html_id,input_name,True)
-    print country_tag[0]
     if country_tag[0] != "<":
-        state_tag = ""
+        region_tag = ""
     else:
-        state_tag = region_select(selected,"","region_selection","region")
+        if selected=="usa":
+            default_region="az"
+        elif selected=="can":
+            default_region="ab"
+        else:
+            default_region=""
         
-    html="<div class='form-label pull-left'>"
-    html="%s<label for='%s'>Country</label></div>%s" %(html,html_id,country_tag)
-    html="%s<div class='clear'></div>" % html
-    html="%s<div class='form-label pull-left'>" % html
-    html="%s<label for='region_selection'>Region</label>" % html
-    html="%s</div>%s" % (html,state_tag)
+        region_tag= region_select(selected,default_region,"region_selection","region")
+
+    html = country_tag+region_tag
     return html
     
 @register.simple_tag
 def country_select(selected="usa", html_id="", input_name="country",
-                   child_regions=False):
+                   child_regions=False,inc_struc=True):
     """
     Builds an html select list of countries. The select list is built using
     data stored on a CDN.
@@ -34,23 +43,37 @@ def country_select(selected="usa", html_id="", input_name="country",
     :html_id:       html id to use for the element
     :input_name:    the form element name
     :child_regions: whether this field has a dependent regions field
+    :inc_struc:     whether or not to include the html label and structure
     
     Returns:        
     :html_str:      HTML <select> block
     
     """
     data_url = 'http://js.nlx.org/myjobsdata/countries_full.json'
-    country_list = _load_json_data(data_url)
+    data_list = _load_json_data(data_url)
+    country_list = data_list["countries"];
+    try:
+        label = data_list["friendly_label"]
+    except KeyError:
+        label = "Country"        
     
     if child_regions:
-        html_str = _build_select_list(country_list,selected,input_name,html_id,
-                                      "hasRegions")
+        sel_tag = _build_select_list(country_list,selected,input_name,html_id,
+                                     "hasRegions")
     else:
-        html_str = _build_select_list(country_list,selected,input_name,html_id)
+        sel_tag = _build_select_list(country_list,selected,input_name,html_id)
+    if inc_struc:
+        html_str="<div class='form-label pull-left'>"
+        html_str="%s<label for='%s'>%s</label></div>" % (html_str,html_id,label)
+        html_str="%s%s<div class='clear'></div>" % (html_str,sel_tag)
+    else:
+        html_str = sel_tag
+        
     return html_str
 
 @register.simple_tag
-def region_select(country="usa",selected="in",html_id="",input_name="region"):
+def region_select(country="usa",selected="az",html_id="",input_name="region",
+                  inc_struc=True):
     """
     Builds an html select list of regions.
     Inputs:
@@ -58,23 +81,33 @@ def region_select(country="usa",selected="in",html_id="",input_name="region"):
     :selected:      default key to select
     :html_id:       html id to use for the element
     :input_name:    the form element name
+    :inc_struc:     whether or not to include the html label and structure
     
     Returns:        
     :html_str:      HTML <select> block or error message
     
     """
     
-    #there is a single file per country, else it rturns null.
+    #there is a single file per country, else it returns null.
     data_url = 'http://js.nlx.org/myjobsdata/%s_regions.json' % country
-    region_list = _load_json_data(data_url)
+    data_list = _load_json_data(data_url)
+    region_list = data_list["regions"]
+    try:
+        label = data_list["friendly_label"]
+    except KeyError:
+        label = "Region" 
+        
     if "error" in region_list:
         html_str = "<input type='hidden' name='%s' value=''>" % input_name
         html_str = "%sNo regions available for the selected country" % html_str
+    elif inc_struc:
+        sel_tag = _build_select_list(region_list,selected,input_name,html_id)
+        html_str="<div class='form-label pull-left'>"
+        html_str="%s<label for='%s'>%s</label></div>" %(html_str,html_id,label)
+        html_str="%s%s<div class='clear'></div>" % (html_str,sel_tag)
     else:
         html_str = _build_select_list(region_list,selected,input_name,html_id)
         
-    
-    print data_url
     return html_str 
 
 # utility function below here
@@ -94,7 +127,7 @@ def _load_json_data(json_url):
         opener = urllib2.build_opener()
         json_data = json.load(opener.open(data))
     except (urllib2.HTTPError,ValueError):
-        return {"error":"There was an error loading the file."}   
+        return {"code":"error","error":"There was an error loading the file."}   
     return json_data
     
 def _build_select_list(select_dict,selected,input_name,html_id,class_name=""):
@@ -115,16 +148,18 @@ def _build_select_list(select_dict,selected,input_name,html_id,class_name=""):
     
     if class_name:
         class_name = " class='%s'" % class_name
-    html_str = "<select name='%s' id='%s'%s>" % (input_name,html_id,class_name)    
-    for k,v in select_dict.iteritems():
-        if k == "error":
-            return v
+    html_str = "<select name='%s' id='%s'%s>" % (input_name,html_id,class_name)
+    html_str = "%s<option value=''></option>" % html_str
+    
+    print selected
+    for item in select_dict:
+        if item["code"] == "error":
+            return item["error"]
         select_flag = ""
-        if k.lower() == selected.lower():
+        if item["code"].lower() == selected.lower():
             select_flag = " SELECTED"
-        if k != "":
-            option_attr = "value='%s'%s" % (k, select_flag)
-            html_str = "%s<option %s>%s</option>" % (html_str,option_attr,v)
-            
+        if item["code"] != "":
+            option_attr = "value='%s'%s" % (item["code"], select_flag)
+            html_str = "%s<option %s>%s</option>" % (html_str,option_attr,item["name"])
     html_str = "%s</select>" % html_str    
     return html_str
