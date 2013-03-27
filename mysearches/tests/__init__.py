eaec4465e64f0ce44d4f8a2589bd9e34ebd1b083
tdphillips <troy@directemployersfoundation.org>
2013-03-27 14:51:09 -0400

    Remove unnecessary view; Remove view from urls; Add unit tests; All tests pass [MJ-131]

--- mysearches/tests/__init__.py
+++ mysearches/tests/__init__.py
@@ -1,3 +1,4 @@
 from mysearches.tests.forms import *
 from mysearches.tests.helpers import *
+from mysearches.tests.views import *
 from django.test.client import Client
--- mysearches/tests/views.py
+++ mysearches/tests/views.py
@@ -3,3 +3,19 @@ from django.test import TestCase
 from myjobs.tests.views import TestClient
 from myjobs.tests.factories import UserFactory
 
+from mysearches import forms
+
+class MySearchViewTests(TestCase):
+    def setUp(self):
+        super(MySearchViewTests, self).setUp()
+        self.client = TestClient()
+        self.user = UserFactory()
+        self.client.login_user(self.user)
+
+    def test_search_main(self):
+        response = self.client.get('/saved-search/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'mysearches/saved_search_main.html')
+        self.assertTemplateUsed(response, 'includes/form-error-highlight.html')
+        self.failUnless(isinstance(response.context['form'], forms.DigestForm));
+        self.failUnless(isinstance(response.context['add_form'], forms.SavedSearchForm));
