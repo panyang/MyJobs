c0f760a426c3d6bed9a3e045d1093be84ccc98eb
tdphillips <troy@directemployersfoundation.org>
2013-04-10 11:35:36 -0400

    Add documentation; Resolve error preventing modal background from being hidden [MJ-170]

--- static/profile.js
+++ static/profile.js
@@ -4,16 +4,38 @@ $(function() {
 
         events: {
             "click [id$='section']": "addSection",
+            // targets buttons within #moduleBank
+
             "click [id$='add']": "editForm",
-            "click [id$='cancel']": "cancelForm",
-            "hidden #edit_modal": "cancelForm",
+            // targets "Add Another" buttons in each module section
+
             "click [id$='edit']": "editForm",
+            // targets "Edit" buttons for individual modules
+
+            "click [id$='cancel']": "cancelForm",
+            // targets "Cancel" button in the modal window
+
+            "hidden #edit_modal": "cancelForm",
+            // targets event fired when #edit_modal is closed by means other
+            // than the cancel button
+
             "click [id$='save']": "saveForm",
+            // targets "Save" button  in the modal window
+
             "click [id$='delete']": "deleteItem",
+            // targets "Delete" buttons for individual modules
         },
 
+        /*
+        Opens an empty module section (Name, Secondary Email, etc),
+        allowing the user to add new modules to it
+
+        :e: button contained within #moduleBank
+        */
         addSection: function(e) {
             e.preventDefault();
+
+            // id is formatted [module_type]-[item_id]
             var module = $(e.target).attr('id').split('-')[0];
             $.ajax({
                 url: '/profile/section/',
@@ -25,33 +47,58 @@ $(function() {
             });
         },
 
+        /*
+        Returns document to the state it was in prior to opening the form modal
+        Called on "Cancel" button click or closure of the modal window
+
+        :e: "Cancel" button within a modal or the modal itself
+        */
         cancelForm: function(e) {
             e.preventDefault();
+
             var target;
             if (e.target.tagName.toLowerCase() == 'a') {
+                // e is the modal cancel button; target it
                 target = $(e.target)
             } else {
-                target = $(e.target).parents('.formBox').find('tr:hidden')
+                // e is the modal itself; target its cancel button instead
+                target = $(e.target).find('a[id$="cancel"]');
             }
+            // id is formatted [module_type]-[item_id]-[event]
             var module = target.attr('id').split('-')[0];
             var item_id = target.attr('id').split('-')[1];
+
+            // Upon closing the modal window, it should be removed from the
+            // document to allow for additional modals
             $("div#edit_modal").remove();
-            var action;
             if (item_id != 'new') {
+                // When "Edit" was clicked, the relevant item was hidden
+                // Since nothing has changed, the item needs to be re-shown
                 $('#'+module+'-'+item_id+'-item').show();
             }
         },
 
+        /*
+        Retrieves the desired form via AJAX, adds the modal form to the
+        document, and displays the form
+
+        :e: "Add Another" button for the current module section
+             or "Edit" button associated with the item to be edited
+        */
         editForm: function(e) {
             e.preventDefault();
 
+            // id is formatted [module_type]-[item_id]-[event]
             var module = $(e.target).attr('id').split("-")[0];
             var id = $(e.target).attr('id').split("-")[1];
             var item;
             if (id != 'new') {
-                item = $(e.target).parent().parent();
+                // targets the table row containing the item to be edited
+                item = $(e.target).parents('tr');
             }
-            var button = $(e.target).parents('.formBox').children('button')
+
+            // targets the "Add Another" button for the current module section
+            var button = $(e.target).parents('.formBox').children('button[id$="add"]')
             $.ajax({
                 url: '/profile/form/',
                 data: {'module':module, 'id':id},
@@ -66,11 +113,22 @@ $(function() {
             });            
         },
 
+        /*
+        Saves both new and edited modules
+
+        :e: "Save" button within a modal
+        */
         saveForm: function(e) {
             e.preventDefault();
+
+            // id is formatted [module_type]-[item_id]-[event]
             var module =  $(e.target).attr('id').split('-')[0];
             var item_id = $(e.target).attr('id').split('-')[1];
+
+            // targets the form contained in the modal window
             var form = $('#edit_modal form');
+
+            // targets the item table in the current module section
             var table = $(e.target).parents('.formBox').children('table')
 
             csrf_token_tag = document.getElementsByName('csrfmiddlewaretoken')[0];
@@ -84,26 +142,34 @@ $(function() {
                 first_instance = 1;
             }  
             console.log(first_instance)    
-            var serialized_data = form.serialize() + '&module=' + module + '&id=' + item_id + '&first_instance=' + first_instance + '&csrfmiddlewaretoken=' + csrf_token;
+            var serialized_data = form.serialize();
+            serialized_data += '&module=' + module + '&id=' + item_id +
+                               '&first_instance=' + first_instance +
+                               '&csrfmiddlewaretoken=' + csrf_token;
             $.ajax({
                 type: 'POST',
                 url: '/profile/form/',
                 data: serialized_data,
                 success: function(data) {
                     if (data.indexOf('<td>') >= 0) {
+                        // form was valid; data should be appended to the table
                         if (first_instance) {
                             $(e.target).parents('.formBox').children('h4').after(
                                 '<table class="table table-bordered table-striped"></table>'
-                                );
+                            );
                             table = $(e.target).parents('.formBox').children('table')
                         }
                         table.append(data);
                         $('#'+module+'-'+item_id+'-item').remove();
                         $('#edit_modal').modal('hide');
                     } else {
+                        // form was a json-encoded list of errors
                         var json = jQuery.parseJSON(data);
+
+                        // remove current errors
                         $('[class*=label-important]').remove();
                         for (var i=0; i<json.length; i++) {
+                            // insert new errors after the relevant inputs
                             $('[id$="-'+json[i]+'"]').after('<span class="label label-important">Required</span>');
                         }
                     }
@@ -111,6 +177,11 @@ $(function() {
             });            
         },
 
+        /*
+        Deletes the specified item
+
+        :e: "Delete" button associated with the item to be deleted
+        */
         deleteItem: function(e) {
             e.preventDefault();
             csrf_token_tag = document.getElementsByName('csrfmiddlewaretoken')[0];
@@ -119,7 +190,10 @@ $(function() {
                 csrf_token = document.getElementsByName('csrfmiddlewaretoken')[0].value;
             }
 
-            var item = $(e.target).parent().parent();
+            // targets the table row containing the item to be deleted
+            var item = $(e.target).parents('tr');
+
+            // id is formatted [module_type]-[item_id]-[event]
             var module = $(e.target).attr('id').split("-")[0];
             var id = $(e.target).attr('id').split("-")[1];
             $.ajax({
@@ -130,18 +204,21 @@ $(function() {
                     parent = item.parents("table");
                     item.remove();
                     if (parent.find("tr").length <=1 ){
-                        parent_name = parent.parents(".formBox").attr("id").split("_")[0] 
+                        // The last item in a module section was deleted
+
+                        // id is formatted [module_type]-items
+                        parent_name = parent.parents(".formBox").attr("id").split("_")[0];
+
+                        // Remove the empty section
                         parent.parents(".formBox").remove();
+
+                        // Replace the button within the #moduleBank table
                         $("#moduleBank table").append(
                             "<tr class='profile_section'><td><a id='"+parent_name+"-section' href=''>"+parent_name+"</a></td></tr>"
                         );
                     }
                 }
             });
-        },
-
-        closeEdit: function(e) {
-            e.preventDefault();
         },
     });
 
