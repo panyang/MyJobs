a51d542c25a730a0680ba34a372e046e199c1d30
Ani Chan <ani.chan844@gmail.com>
2013-04-16 10:58:47 -0400

    Add path for more feed results back into urls.py

--- mysearches/urls.py
+++ mysearches/urls.py
@@ -6,6 +6,7 @@ from mysearches.views import *
 urlpatterns = patterns('',
     url(r'^delete/(?P<search_id>\d+)/$', delete_saved_search, name='delete_saved_search'),
     url(r'^$', saved_search_main, name='saved_search_main'),
+    url(r'^more-results/$', more_feed_results, name='more_feed_results'),
     url(r'^(?P<search_id>\d+)/$', view_full_feed, name='view_full_feed'),
     url(r'^validate-url$', validate_url, name='validate_url'),
     url(r'^save-digest$', save_digest_form, name='save_digest_form'),
