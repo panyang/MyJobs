b3a32621001400c3305ad67036bb0a94e687c055
michaelgzoller <mzoller@directemployersfoundation.org>
2013-04-17 16:09:06 -0400

    Update models MJ-216

--- mymessages/models.py
+++ mymessages/models.py
@@ -3,9 +3,9 @@ from myjobs.models import User
 
 class Message(models.Model):
    subject = models.CharField(max_length=255, blank=True)
-   text = models.TextField(blank=True) 
+   message_body = models.TextField(blank=True) 
    date_sent = models.DateTimeField(auto_now_add=True)
    date_read = models.DateTimeField(null=True)
    recipient = models.ForeignKey(User, editable=False)
    sender = models.CharField(max_length=255, blank=True)
-   priority = models.PositiveSmallIntegerField()
+   priority = models.PositiveIntegerField()
