51cd2bbf1011149c2a3bb78d1eb27922b56ad708
michaelgzoller <mzoller@directemployersfoundation.org>
2013-04-17 15:30:44 -0400

    Add models MJ-216

--- mymessages/models.py
+++ mymessages/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from myjobs.models import User
 
 class Message(models.Model):
-    
+   subject = models.CharField(max_length=255, blank=True)
+   text = models.TextField(blank=True) 
+   date_sent = models.DateTimeField(auto_now_add=True)
+   date_read = models.DateTimeField(null=True)
+   recipient = models.ForeignKey(User, editable=False)
+   sender = models.CharField(max_length=255, blank=True)
+   priority = models.PositiveSmallIntegerField()
