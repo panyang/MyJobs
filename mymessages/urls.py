034fd0fd2da5ce79dcecf27e33c20568e56f1a5f
michaelgzoller <mzoller@directemployersfoundation.org>
2013-04-23 15:08:45 -0400

    Add URLs for inbox MJ-185

---
+++ mymessages/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls.defaults import patterns, url, include
+from django.http import HttpResponsePermanentRedirect
+
+urlpatterns = patterns('MyJobs.mymessages.views',
+    url(r'^messages/$', 'messages', name='messages'),
+    url(r'^message/new$', 'new_message', name='new_message'),
+    url(r'^message/(?P<mail_id>)/$', 'read_message', name='read_message'),
+    url(r'^message/(?P<mail_id>)/delete$', 'delete_message', 
+        name='delete_message'),
+    url(r'^send/$', 'continue_sending_mail', name='continue_sending_mail'),
+)
