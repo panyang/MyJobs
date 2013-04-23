34ca1f467a1a6cf4506b131272a54784cf970c1e
michaelgzoller <mzoller@directemployersfoundation.org>
2013-04-23 15:43:39 -0400

    Views skeletons MJ-185

--- mymessages/views.py
+++ mymessages/views.py
@@ -1,1 +1,18 @@
-# Create your views here.
+@login_required
+def messages(request):
+	pass
+
+
+@login_required
+def new_message(request):
+	pass
+
+
+@login_required
+def read_message(request):
+	pass
+
+
+@login_required
+def delete_message(request):
+	pass
