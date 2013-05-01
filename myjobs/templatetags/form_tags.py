01acb193c25bfd32ef42560a5febf2942ef95c0f
JustinDennis <justin@directemployersfoundation.org>
2013-05-01 15:48:05 -0400

    fixed ux display for yes/no instead of true/false MJ-210

--- myjobs/templatetags/form_tags.py
+++ myjobs/templatetags/form_tags.py
@@ -11,3 +11,15 @@ def is_boolean_field(field):
 @register.assignment_tag
 def is_select_field(field):
     return type(field.field) == TypedChoiceField
+    
+@register.filter(name='lookup')
+def get_value(value):
+    value_lookup = {
+        "True": "Yes",
+        "False": "No"
+    }
+    if value in value_lookup:
+        return value_lookup[value]
+    
+    return value
+    
