abb0b2ca2b86a99ce709abb3e2274f1a868dd177
tdphillips <troy@directemployersfoundation.org>
2013-05-01 16:17:57 -0400

    Add search creation api and tests [MJ-212]

--- myjobs/api.py
+++ myjobs/api.py
@@ -1,9 +1,12 @@
+import datetime
+
 from django.core import serializers
 from django.db import IntegrityError
 from django.conf.urls import url
 from django.conf.urls.defaults import patterns, url
 from django.contrib.auth import authenticate, login
 
+from tastypie import fields
 from tastypie.authorization import Authorization
 from tastypie.authentication import ApiKeyAuthentication
 from tastypie.exceptions import BadRequest
@@ -11,6 +14,8 @@ from tastypie.resources import ModelResource
 from tastypie.utils import trailing_slash
 
 from myjobs.models import User
+from mysearches.helpers import validate_dotjobs_url
+from mysearches.models import SavedSearch
 from registration import signals as custom_signals
 
 class UserResource(ModelResource):
@@ -35,3 +40,45 @@ class UserResource(ModelResource):
             return bundle
         except IntegrityError:
             raise BadRequest('That username already exists')
+
+class SavedSearchResource(ModelResource):
+    user = fields.ForeignKey(UserResource, 'user')
+    class Meta:
+        queryset = SavedSearch.objects.all()
+        resource_name = 'savedsearch'
+        authorization = Authorization()
+        list_allowed_methods = ['post']
+        detail_allowed_methods = []
+        authentication = ApiKeyAuthentication()
+        always_return_data = True
+
+    def obj_create(self, bundle, **kwargs):
+        ur = UserResource()
+        user_bundle = ur.build_bundle()
+        user_bundle.data['email'] = bundle.data.get('email')
+        user_bundle = ur.obj_create(user_bundle)
+
+        label, feed = validate_dotjobs_url(bundle.data.get('url'))
+        if not (label and feed):
+            return BadRequest('This is not a valid .JOBS feed')
+
+        notes = bundle.data.get('notes', '')
+        if not notes:
+            # Monday, April 29, 2013 10:26 AM
+            now = datetime.datetime.now().strftime('%A, %B %d, %Y %H:%M %p')
+            notes += 'Sent on ' + now
+            if bundle.request:
+                notes += ' from ' + bundle.request.get_host()
+
+        search_args = {'url': bundle.data.get('url'),
+                       'label': label,
+                       'feed': feed,
+                       'user': user_bundle.obj,
+                       'email': bundle.data.get('email'),
+                       'frequency': bundle.data.get('frequency', 'D'),
+                       'day_of_week': bundle.data.get('day_of_week'),
+                       'day_of_month': bundle.data.get('day_of_month'),
+                       'notes': notes}
+        search = SavedSearch.objects.create(**search_args)
+        bundle.obj = search
+        return bundle
---
+++ myjobs/tests/api.py
@@ -0,0 +1,49 @@
+import simplejson as json
+
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+
+from tastypie.models import create_api_key
+
+from myjobs.models import User
+from myjobs.tests.factories import UserFactory
+from myjobs.tests.views import TestClient
+from myprofile.models import SecondaryEmail
+from mysearches.models import SavedSearch
+
+class ApiTests(TestCase):
+    def setUp(self):
+        super(ApiTests, self).setUp()
+        self.user = UserFactory()
+        self.client = TestClient()
+        self.data = {'email':'alice@example.com', 'url':'jobs.jobs/jobs'}
+        create_api_key(User, instance=self.user, created=True)
+
+    def test_post_new_search_existing_user(self):
+        response = self.client.post('/api/v1/savedsearch/', data=json.dumps(self.data), format='jsonp', content_type='application/json', HTTP_AUTHORIZATION='ApiKey %s:%s' % (self.user.email, self.user.api_key.key))
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(SavedSearch.objects.count(), 1)
+        search = SavedSearch.objects.all()[0]
+        self.assertEqual(search.user, self.user)
+        self.assertTrue('testserver' in search.notes)
+
+    def test_post_new_search_new_user(self):
+        self.data['email'] = 'new@example.com'
+        response = self.client.post('/api/v1/savedsearch/', data=json.dumps(self.data), format='jsonp', content_type='application/json', HTTP_AUTHORIZATION='ApiKey %s:%s' % (self.user.email, self.user.api_key.key))
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(SavedSearch.objects.count(), 1)
+        self.assertEqual(User.objects.count(), 2)
+        search = SavedSearch.objects.all()[0]
+        user = User.objects.get(email='new@example.com')
+        self.assertEqual(search.user, user)
+
+    def test_post_new_search_secondary_email(self):
+        SecondaryEmail.objects.create(user=self.user, email='secondary@example.com')
+        self.data['email'] = 'secondary@example.com'
+        response = self.client.post('/api/v1/savedsearch/', data=json.dumps(self.data), format='jsonp', content_type='application/json', HTTP_AUTHORIZATION='ApiKey %s:%s' % (self.user.email, self.user.api_key.key))
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(SavedSearch.objects.count(), 1)
+        self.assertEqual(User.objects.count(), 1)
+        search = SavedSearch.objects.all()[0]
+        self.assertEqual(search.user, self.user)
+        self.assertEqual(search.email, 'secondary@example.com')
