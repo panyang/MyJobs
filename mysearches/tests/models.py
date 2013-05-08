84e2555a88ccd1d3e3e013f90c943b718aa28e36
tdphillips <troy@directemployersfoundation.org>
2013-05-08 14:01:31 -0400

    Changes "from" address for saved search emails; All tests pass [MJ-244]

---
+++ mysearches/tests/models.py
@@ -0,0 +1,32 @@
+from django.core import mail
+from django.test import TestCase
+
+from myjobs.tests.factories import UserFactory
+from mysearches.models import SavedSearch, SavedSearchDigest
+from mysearches.tests.factories import SavedSearchFactory, SavedSearchDigestFactory
+
+class SavedSearchModelsTests(TestCase):
+    def setUp(self):
+        self.user = UserFactory()
+
+    def test_send_search_email(self):
+        search = SavedSearchFactory(user=self.user)
+        search.send_email()
+        self.assertEqual(len(mail.outbox), 1)
+
+        email = mail.outbox.pop()
+        self.assertEqual(email.from_email, 'savedsearch@my.jobs')
+        self.assertEqual(email.to, [self.user.email])
+
+    def test_send_search_digest_email(self):
+        digest = SavedSearchDigestFactory(user=self.user)
+        digest.send_email()
+        self.assertEqual(len(mail.outbox), 0)
+
+        SavedSearchFactory(user=self.user)
+        digest.send_email()
+        self.assertEqual(len(mail.outbox), 1)
+
+        email = mail.outbox.pop()
+        self.assertEqual(email.from_email, 'savedsearch@my.jobs')
+        self.assertEqual(email.to, [self.user.email])
