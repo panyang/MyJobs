ef24fd29a0df66121b982b4def9aa25a34cdeaa3
Ani Chan <ani.chan844@gmail.com>
2013-05-14 12:24:37 -0400

    Account settings js

---
+++ static/account-settings.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+    $(window).on("resize", function(e) {
+        if ($(window).width() < 500) {
+            $('div.settings-nav').show();
+            $('div.account-settings').hide();
+        } else {
+            $('div.settings-nav').show();
+            $('div.account-settings').show();
+        }
+    });
+});
+
+$(function() {
+    var AppView = Backbone.View.extend({
+        el: $("body"),
+        events: {
+            "click [id^='account-']": "showSection",
+            "submit form": "saveForm",
+            "click .show-captcha-modal": "captchaModal",
+        },
+        
+        showSection: function(e) {
+            e.preventDefault();
+            var section_name = $(e.target).parents('a').attr('id').split('-')[1];
+            $.ajax({
+                url: "/edit/" + section_name,
+                success: function(data) {
+                    $('div.account-settings').html(data);
+                }
+            });
+        },
+
+        saveForm: function(e) {
+            e.preventDefault();
+            var section_name = $(e.target).attr('id').split('-')[1];
+            $.ajax({
+                type: "POST",
+                data: $(e.target).serialize(),
+                url: "/edit/" + section_name,
+                success: function(data) {
+                    if (data == "success") {
+                        $('.form-status').html('<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button>Your information has been updated.</div>');
+                    } else {
+                        $('div.account-settings').html(data);
+                    }
+                }
+            });
+                  
+        },
+        
+        captchaModal: function(e) {
+            e.preventDefault();
+            $.ajax({
+                type:"POST",
+                url: "/edit/delete",
+                data: $("#captcha-form").serialize(),
+                success: function(data) {
+                    if (data == 'success') {
+                        $("#captcha-errors").html('');
+                        $("#captcha_modal").modal();
+                    } else {
+                        var error = jQuery.parseJSON(data)[0][0];
+                        $("#captcha-errors").html('<div class="alert-message block-message error">'+error+'</div>');
+                    }
+                }
+            });
+        },
+
+    });
+
+    var App = new AppView;
+});
