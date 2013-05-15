4b80490d7594e15ec7ae874a8465c01cbcadaeac
tdphillips <troy@directemployersfoundation.org>
2013-05-15 13:20:41 -0400

    Correct auto-created notes for saved searches [MJ-263]

--- myjobs/tests/api.py
+++ myjobs/tests/api.py
@@ -87,7 +87,8 @@ class SavedSearchResourceTests(TestCase):
         self.assertEqual(SavedSearch.objects.count(), 1)
         search = SavedSearch.objects.all()[0]
         self.assertEqual(search.user, self.user)
-        self.assertTrue('testserver' in search.notes)
+        print search.notes
+        self.assertTrue('jobs.jobs' in search.notes)
         content = json.loads(response.content)
         self.assertEqual(len(content), 3)
 
