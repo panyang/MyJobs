5fbf724d5133e0a659e84fa492bee833842e96b7
tdphillips <troy@directemployersfoundation.org>
2013-05-16 13:45:05 -0400

    Allow saved searches to display in admin [MJ-274]

--- myjobs/admin.py
+++ myjobs/admin.py
@@ -3,7 +3,9 @@ from django.contrib.auth.models import Group
 
 from myjobs.models import User
 from registration.models import ActivationProfile
+from mysearches.models import SavedSearch
 
 admin.site.register(User)
 admin.site.register(ActivationProfile)
 admin.site.unregister(Group)
+admin.site.register(SavedSearch)
