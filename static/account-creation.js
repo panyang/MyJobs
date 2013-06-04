02769788cd0c04b935a94b096df8e54e363f6dcd
tdphillips <troy@directemployersfoundation.org>
2013-06-04 09:24:07 -0400

    Change datepicker's default format for dates [MJ-343]

--- static/account-creation.js
+++ static/account-creation.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     }
 
     $(function() {
-        $( "input[id$='date']" ).datepicker({dateFormat: "yy-mm-dd",
+        $( "input[id$='date']" ).datepicker({dateFormat: "mm/dd/yy",
                                              constrainInput: false});
     });
     // perform display modifications for fields
