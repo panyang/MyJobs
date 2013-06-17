faefb4438650fe2fe93e579993d31f2fd199f9f1
tdphillips <troy@directemployersfoundation.org>
2013-06-17 14:15:05 -0400

    Make activity test page not load anything until requested by the user [MJ-369]

--- myactivity/views.py
+++ myactivity/views.py
@@ -16,40 +16,42 @@ def activity_search_feed(request):
     Returns a list of users who created a saved search on the given microsite
     between the given (optional) dates
     """
-    url = request.REQUEST.get('microsite')
-    if url:
-        if url.find('//') == -1:
-            url = '//' + url
-        microsite = urlparse(url).netloc
-    else:
-        microsite = 'indiana.jobs'
+    data = {}
+    if request.method == 'POST':
+        url = request.REQUEST.get('microsite')
+        if url:
+            if url.find('//') == -1:
+                url = '//' + url
+            microsite = urlparse(url).netloc
+        else:
+            microsite = 'indiana.jobs'
+        data['site'] = microsite
 
-    # Saved searches were created after this date...
-    after = request.REQUEST.get('after')
-    if after:
-        after = datetime.strptime(after, '%Y-%m-%d')
-    else:
-        # Defaults to one week ago
-        after = datetime.now() - timedelta(days=7)
-    # ... and before this one
-    before = request.REQUEST.get('before')
-    if before:
-        before = datetime.strptime(before, '%Y-%m-%d')
-    else:
-        # Defaults to the date and time that the page is accessed
-        before = datetime.now()
+        # Saved searches were created after this date...
+        after = request.REQUEST.get('after')
+        if after:
+            after = datetime.strptime(after, '%Y-%m-%d')
+        else:
+            # Defaults to one week ago
+            after = datetime.now() - timedelta(days=7)
+        # ... and before this one
+        before = request.REQUEST.get('before')
+        if before:
+            before = datetime.strptime(before, '%Y-%m-%d')
+        else:
+            # Defaults to the date and time that the page is accessed
+            before = datetime.now()
+        data['after'] = after
+        data['before'] = before
 
-    # Prefetch the user
-    searches = SavedSearch.objects.select_related('user')
+        # Prefetch the user
+        searches = SavedSearch.objects.select_related('user')
 
-    # All searches saved from a given microsite
-    searches = searches.filter(url__contains=microsite)
+        # All searches saved from a given microsite
+        searches = searches.filter(url__contains=microsite)
 
-    # Specific microsite searches saved between two dates
-    searches = searches.filter(created_on__range=[after, before])
-    data = {'searches': searches,
-            'site': microsite,
-            'after': after,
-            'before': before}
+        # Specific microsite searches saved between two dates
+        searches = searches.filter(created_on__range=[after, before])
+        data['searches'] = searches
     return render_to_response('myactivity/activity_feed.html', data,
                               RequestContext(request))
