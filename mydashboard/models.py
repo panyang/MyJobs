2feb4b486b69bbc39e9084244215b874c9aae196
michaelgzoller <mzoller@directemployersfoundation.org>
2013-06-17 15:12:33 -0400

    Company object MJ-300

--- mydashboard/models.py
+++ mydashboard/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from myjobs.models import *
+from myprofile.models import *
+
+
+class Company(models.Model):
+    """
+    Companies are the central hub for a group of modules and admins (Users).
+
+    """
+    name = models.CharField()
+    admins = models.ManyToManyField(User, through='Administrators')
+
+class DashboardModule(models.Model):
+    company = models.ForeignKey(Company)
+
+class Administrators(models.Model):
+    user = models.ForeignKey(User)
+    company = models.ForeignKey(Company)
+    date_added = models.DateTimeField(auto_now=True)
+    added_by = models.ForeignKey(User)
