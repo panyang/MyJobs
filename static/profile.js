319609d0b0a4d18c31f32814fd588ba1deeaab24
almathew <ashley@directemployersfoundation.org>
2013-06-18 11:23:41 -0400

    End date no longer shows up when editing existing job [MJ-322]

--- static/profile.js
+++ static/profile.js
@@ -111,7 +111,11 @@ $(function() {
                     data: {'module':module, 'id':id},
                     success: function(data) {
                         $('#moduleColumn').append(data);
-
+                        // Prevents end date from showing up if "I still work here" is checked
+                        if(($("[id$='id_employmenthistory-current_indicator']").is(":checked"))) {
+                            $("[id$='id_employmenthistory-end_date']").hide();
+                            $("[for$='id_employmenthistory-end_date']").hide();
+                        }
                         $('#edit_modal').modal();
                         datepicker();
                     }
