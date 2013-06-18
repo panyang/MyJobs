404599af6c60cd8f4cbc7761a16f4371ee96383a
michaelgzoller <mzoller@directemployersfoundation.org>
2013-06-18 16:10:36 -0400

    Add id field as primary key MJ-300

--- mydashboard/models.py
+++ mydashboard/models.py
@@ -9,6 +9,11 @@ class Company(models.Model):
     Companies are the central hub for a group of modules and admins (Users).
 
     """
+    # Why isn't the primary key an auto incrementing field?
+    # To keep the my.jobs Company model in sync with the microsites Company
+    # model the id is the same to ease the transition to a single model down the
+    # line.
+    id = models.IntegerField(primary_key=true)
     name = models.CharField()
     admins = models.ManyToManyField(User, through='Administrators')
 
