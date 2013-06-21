33dbf4af74198f10d062713cdd38faba0cae1e5a
tdphillips <troy@directemployersfoundation.org>
2013-06-21 14:56:04 -0400

    Miscellaneous fixes [MJ-232]
    Generalized template tag
    Removed confusing table styles
    Turned the details table into a collection of divs and made it a formbox
    Truncated the search url to prevent it from breaking layout
    Added target=_blank to links

--- mysearches/templatetags/label_tags.py
+++ mysearches/templatetags/label_tags.py
@@ -3,22 +3,37 @@ from django.utils.safestring import mark_safe
 
 register = template.Library()
 
-@register.filter(name='make_active_label')
-def make_active_label(active, extra_classes=''):
+@register.simple_tag(name='make_label')
+def make_label(active, extra_classes='', **kwargs):
     """
     Creates a Bootstrap label in green or gray
 
     Inputs:
-    :active: flag; True: green label; False: gray label
+    :active: flag; True: success color; False: fail color
     :extra_classes: optional string containing custom classes
         for the label; pull-left, pull-right, etc
+    :true: string representing the desired label to be used
+        if :active: == True
+        Default: "success" - results in green label
+    :false: string representing the desired label to be used
+        if :active: == False
+        Default: "default" - results in gray label
+    :true_msg: text for label when :active: == True
+        Default: "Active"
+    :false_msg: text for label when :active: == False
+        Default: "Inactive"
 
     Outputs:
     :label: safe HTML string representing a Bootstrap label
     """
+    true = kwargs.pop('true', 'success')
+    true_msg = kwargs.pop('true_msg', 'Active')
+    false = kwargs.pop('false', 'default')
+    false_msg = kwargs.pop('false_msg', 'Inactive')
+
     div = '<div class="label label-%s %s">%s</div>'
     if active == True:
-        div %= ('success', extra_classes, 'Active')
+        div %= (true, extra_classes, true_msg)
     else:
-        div %= ('default', extra_classes, 'Inactive')
+        div %= (false, extra_classes, false_msg)
     return mark_safe(div)
