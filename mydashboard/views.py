db8ca138bf8786ec4f9963ba020ef43ad83a5ff4
JustinDennis <justin@directemployersfoundation.org>
2013-06-24 15:26:12 -0400

    updated views MJ-394

--- mydashboard/views.py
+++ mydashboard/views.py
@@ -14,73 +14,36 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView
 
 from myjobs.models import User, EmailLog
+from mydashboard.models import *
 from myjobs.forms import *
 from myjobs.helpers import *
 from myprofile.forms import *
 from registration.forms import *
-
-#def dashboard(request):
-    #return render_to_response('mydashboard/dashboard.html')
-    #render(request, 'mydashboard/dashboard.html')
-    #template_name = "mydashboard/dashboard.html"
-    
+   
 @user_passes_test(User.objects.not_disabled)
 def mydashboard(request):
     """
-    Main profile view that the user first sees. Ultimately generates the
-    following in data_dict:
-
-    :profile_config: A list of dictionaries. Each dictionary represents a
-                     different module (based on module_list) with the keys:
-                     verbose - the displayable title of the module
-                     name - the module name as it's named in the models.
-                     items - all the instances in that module for the user
-
-    :name_obj:       The name of the user or, if not provided, the user's email
+    Notes
     """
 
     settings = {'user': request.user}
-    module_list = ['Name', 'Education', 'EmploymentHistory', 'SecondaryEmail',
-                   'Telephone', 'Address']
-    units = request.user.profileunits_set
-    profile_config = []
-    
-    for module in module_list:
-        model = globals()[module]
-        verbose = model._meta.verbose_name
-
-        x= []
-        module_config = {}
-        module_units = units.filter(content_type__name=verbose)
-
-        module_config['verbose'] = verbose.title()
-        module_config['name'] = module
-        for unit in module_units:
-            if hasattr(unit, module.lower()):
-                x.append(getattr(unit, module.lower()))
-        module_config['items'] = x
         
-        profile_config.append(module_config)
-
-    data_dict = {'profile_config': profile_config,        
-                 'name_obj': get_name_obj(request)}
+    company = Administrators.objects.get(admin=request.user)
+    admins = Administrators.objects.filter(company=company.company)
+    microsites = Microsite.objects.filter(company=company.company)    
+    
+    data_dict = {'company_name': company.company,
+                 'company_microsites': microsites,
+                 'company_admins': admins,}
     
     return render_to_response('mydashboard/mydashboard.html', data_dict,
-                              RequestContext(request))
+                              context_instance=RequestContext(request))
+    
     
 @user_passes_test(User.objects.not_disabled)    
 def profile(request):
     """
-    Main profile view that the user first sees. Ultimately generates the
-    following in data_dict:
-
-    :profile_config: A list of dictionaries. Each dictionary represents a
-                     different module (based on module_list) with the keys:
-                     verbose - the displayable title of the module
-                     name - the module name as it's named in the models.
-                     items - all the instances in that module for the user
-
-    :name_obj:       The name of the user or, if not provided, the user's email
+    Notes
     """
 
     settings = {'user': request.user}
@@ -115,16 +78,7 @@ def profile(request):
 @user_passes_test(User.objects.not_disabled)    
 def microsites(request):
     """
-    Main profile view that the user first sees. Ultimately generates the
-    following in data_dict:
-
-    :profile_config: A list of dictionaries. Each dictionary represents a
-                     different module (based on module_list) with the keys:
-                     verbose - the displayable title of the module
-                     name - the module name as it's named in the models.
-                     items - all the instances in that module for the user
-
-    :name_obj:       The name of the user or, if not provided, the user's email
+    Notes
     """
 
     settings = {'user': request.user}
@@ -159,16 +113,7 @@ def microsites(request):
 @user_passes_test(User.objects.not_disabled)    
 def searches(request):
     """
-    Main profile view that the user first sees. Ultimately generates the
-    following in data_dict:
-
-    :profile_config: A list of dictionaries. Each dictionary represents a
-                     different module (based on module_list) with the keys:
-                     verbose - the displayable title of the module
-                     name - the module name as it's named in the models.
-                     items - all the instances in that module for the user
-
-    :name_obj:       The name of the user or, if not provided, the user's email
+    Notes
     """
 
     settings = {'user': request.user}
@@ -203,16 +148,7 @@ def searches(request):
 @user_passes_test(User.objects.not_disabled)    
 def jobs(request):
     """
-    Main profile view that the user first sees. Ultimately generates the
-    following in data_dict:
-
-    :profile_config: A list of dictionaries. Each dictionary represents a
-                     different module (based on module_list) with the keys:
-                     verbose - the displayable title of the module
-                     name - the module name as it's named in the models.
-                     items - all the instances in that module for the user
-
-    :name_obj:       The name of the user or, if not provided, the user's email
+    Notes
     """
 
     settings = {'user': request.user}
