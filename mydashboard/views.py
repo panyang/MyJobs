c5d3ad5789e3254e5bfadab8e691a598897400c6
JustinDennis <justin@directemployersfoundation.org>
2013-07-10 11:48:24 -0400

    removed view MJ-394

--- mydashboard/views.py
+++ mydashboard/views.py
@@ -131,39 +131,4 @@ def dashboard(request):
                               context_instance=RequestContext(request))
     
 
-@user_passes_test(lambda u: User.objects.is_group_member(u, 'Employer'))
-def candidate_information(request, user_id):
-    # gets returned with response to request
-    data_dict = {}
-    models = {}
-    name = "Name not given"
 
-    # user gets pulled out from id
-    try:
-        user = User.objects.get(id=user_id)
-    except User.DoesNotExist:
-        raise Http404
-
-    if user.opt_in_employers:
-        units = ProfileUnits.objects.filter(user=user)
-
-        for unit in units:
-            if unit.__getattribute__(unit.get_model_name()).is_displayed():
-                models.setdefault(unit.get_model_name(), []).append(
-                unit.__getattribute__(unit.get_model_name()))
-
-        # if Name ProfileUnit exsists
-        if models.get('name'):
-            name=models['name'][0]
-            del models['name']
-
-        searches = SavedSearch.objects.filter(user=user)
-    
-        data_dict = {'user_info': models,
-                     'primary_name': name,
-                     'the_user': user,
-                     'searches': searches}
-    else:
-        raise Http404
-    return render_to_response('myactivity/candidate_information.html', data_dict,
-                            RequestContext(request))
