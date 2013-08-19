11b5cf4dc28576ecaf858d9e0083500b828add17
galitskyd <david@directemployersfoundation.org>
2013-08-19 15:58:31 -0400

    changed microsite to a view (candidates/view/microsite) [MJ-453]

--- mydashboard/urls.py
+++ mydashboard/urls.py
@@ -5,5 +5,5 @@ urlpatterns = patterns('MyJobs.mydashboard.views',
     url(r'^view$', 'dashboard', name='dashboard'),
     url(r'^view/details$', 'candidate_information',
         name='candidate_information'),
-    url(r'^microsite$', 'microsite_activity', name='microsite_activity'),
+    url(r'^view/microsite$', 'microsite_activity', name='microsite_activity'),
 )
