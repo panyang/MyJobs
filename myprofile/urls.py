2b57f05ece313735e5a16ae8b17dfe75482a29f2
galitskyd <david@directemployersfoundation.org>
2013-08-21 14:18:07 -0400

    initial url cleanup [MJ-530]

--- myprofile/urls.py
+++ myprofile/urls.py
@@ -1,12 +1,11 @@
-from django.conf.urls.defaults import patterns, url, include
-from django.contrib import admin
-from django.http import HttpResponsePermanentRedirect
-
-from myjobs.views import *
+from django.conf.urls import patterns, url
+from django.views.generic import RedirectView
 
 urlpatterns = patterns('MyJobs.myprofile.views',
-    url(r'^$', 'edit_profile', name='view_profile'),
-    url(r'^delete/(?P<item_id>\d+)/$', 'delete_item', name='delete_item'),
-    url(r'^edit/$', 'handle_form', name='handle_form'),
-    url(r'^details/$', 'get_details', name='get_details'),
+    url(r'^$', RedirectView.as_view(url='/profile/view/')),
+    url(r'^view/$', 'edit_profile', name='view_profile'),
+    url(r'^view$', 'edit_profile', name='view_profile'),
+    url(r'^view/delete$', 'delete_item', name='delete_item'),
+    url(r'^view/edit$', 'handle_form', name='handle_form'),
+    url(r'^view/details$', 'get_details', name='get_details'),
 )
