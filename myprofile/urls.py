19ecdd8f87f13572b6bfcd9a76aa72df67c822bc
galitskyd <david@directemployersfoundation.org>
2013-08-21 14:56:02 -0400

    profile url normalize [MJ-530]

--- myprofile/urls.py
+++ myprofile/urls.py
@@ -3,8 +3,8 @@ from django.views.generic import RedirectView
 
 urlpatterns = patterns('MyJobs.myprofile.views',
     url(r'^$', RedirectView.as_view(url='/profile/view/')),
-    url(r'^view/$', 'edit_profile', name='view_profile'),
     url(r'^view$', 'edit_profile', name='view_profile'),
+    url(r'^view/$', 'edit_profile', name='view_profile'),
     url(r'^view/delete$', 'delete_item', name='delete_item'),
     url(r'^view/edit$', 'handle_form', name='handle_form'),
     url(r'^view/details$', 'get_details', name='get_details'),
