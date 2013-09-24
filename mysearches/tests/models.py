8a3fac0f4d20ab7735a1fbe393942e92f2706253
fezick <jason@directemployersfoundation.org>
2013-09-24 11:20:05 -0400

    Add tests for initial and update email tasks. MJ-596

--- mysearches/tests/models.py
+++ mysearches/tests/models.py
@@ -15,7 +15,7 @@ class SavedSearchModelsTests(TestCase):
     def test_send_search_email(self):
         search = SavedSearchFactory(user=self.user)
         search = SavedSearchFactory(user=self.user, is_active=False,
-                                    url='jobs.jobs/search?q=new+search')
+                                    url='www.my.jobs/search?q=new+search')
         search.send_email()
         self.assertEqual(len(mail.outbox), 1)
 
@@ -50,3 +50,32 @@ class SavedSearchModelsTests(TestCase):
         SavedSearchFactory(user=self.user)
         digest.send_email()
         self.assertEqual(len(mail.outbox), 1)
+    
+    def test_send_initial_email(self):
+        search = SavedSearchFactory(user=self.user)
+        search = SavedSearchFactory(user=self.user, is_active=False,
+                                    url='www.my.jobs/search?q=new+search')
+        search.send_initial_email()
+        self.assertEqual(len(mail.outbox), 1)
+
+        email = mail.outbox.pop()
+        self.assertEqual(email.from_email, settings.SAVED_SEARCH_EMAIL)
+        self.assertEqual(email.to, [self.user.email])
+        self.assertEqual("My.jobs New Saved Search" in email.subject, True)
+        self.assertTrue("table" in email.body)
+        self.assertTrue(email.to[0] in email.body)
+    
+    def test_send_update_email(self):
+        search = SavedSearchFactory(user=self.user)
+        search = SavedSearchFactory(user=self.user, is_active=False,
+                                    url='www.my.jobs/search?q=new+search')
+        search.send_update_email('Your search is updated')
+        self.assertEqual(len(mail.outbox), 1)
+
+        email = mail.outbox.pop()
+        self.assertEqual(email.from_email, settings.SAVED_SEARCH_EMAIL)
+        self.assertEqual(email.to, [self.user.email])
+        self.assertEqual("My.jobs Saved Search Updated" in email.subject, True)
+        self.assertTrue("table" in email.body)
+        self.assertTrue("Your search is updated" in email.body)
+        self.assertTrue(email.to[0] in email.body)
