3519bbe46d086733b951310fca7fb8a3d59391a5
tdphillips <troy@directemployersfoundation.org>
2013-09-25 16:57:50 -0400

    Fix tracking url [MJ-480]

--- myanalytics/urls.py
+++ myanalytics/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url, patterns
 
 
 urlpatterns = patterns('MyJobs.myanalytics.views',
-    url('track$', 'track', name='track'),
+    url('^track$', 'track', name='track'),
 )
