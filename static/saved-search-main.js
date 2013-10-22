e2344e8106501fc8ac344019e3461ec96b56fccc
tdphillips <troy@directemployersfoundation.org>
2013-10-22 11:07:34 -0400

    Update saved search feed link on mobile [MJ-644]

--- static/saved-search-main.js
+++ static/saved-search-main.js
@@ -51,4 +51,17 @@ $(function() {
     });
 
     var Search = new SearchView;
+
+
+    /*
+    Targets event fired when "Search Name" column is clicked
+    */
+    $('.view_search').click(function(e) {
+        // Only redirect when at mobile resolution (<= 500px)
+        if ($(window).width() <= 500) {
+            // id is formatted saved-search-[id]
+            var id = $(e.target).closest('tr').attr('id').split('-')[2];
+            window.location += 'feed?id=' + id;
+        }
+    });
 });
