3f64cebcbaa9b6b23d8006877c24ad02f85fbb2e
tdphillips <troy@directemployersfoundation.org>
2013-10-30 15:58:40 -0400

    Add braces to uuid generation [MJ-480]

--- cdn_files/myjobs/ma.js
+++ cdn_files/myjobs/ma.js
@@ -765,7 +765,7 @@ if (typeof Piwik !== 'object') {
          * - based on uuid from broofa.com/2008/09/javascript-uuid-function/ (MIT / GPL)
          ************************************************************/
         function uuid4() {
-            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            return '{xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx}'.replace(/[xy]/g, function(c) {
                 var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r&0x3|0x8);
                 return v.toString(16);
             });
